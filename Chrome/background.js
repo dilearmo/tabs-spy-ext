7bec4d161847a0acde3f59cec4e1d973f50af2d0
Diego Leonardo Arias Mora <dilearmo@hotmail.com>
2020-03-26 10:56:44 -0600

    Intento de obtener ventanas minimizadas

--- Chrome/background.js
+++ Chrome/background.js
@@ -23,3 +23,31 @@ function checkTabs() {
 chrome.browserAction.onClicked.addListener(() => {
     checkTabs();
 });
+
+// // Verifica ventanas minimizadas   El principal problema de esto es que esta extensión se ejecuta en todas las pestañas y lo estaría haciendo por cada una
+// function reportMinimizedWindows(windows) {
+//     for (var i = 0; i < windows.length; i++) {
+//         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+//             chrome.tabs.sendMessage(tabs[0].id, {
+//                 type: "minimized_window_msg",
+//                 mnzd_msg: `Window #${windows[i].id} is minimized!`
+//             }, function (response) { });
+//         });
+//     }
+// }
+
+// setInterval(() => {
+//     chrome.windows.getAll(function (windows) {
+//         var minimized = [];
+
+//         for (var i = 0; i < windows.length; i++) {
+//             console.log(`state: ${windows[i].state} and todo: ${windows[i]}`)
+//             if (windows[i].state === "minimized") {
+//                 minimized.push(windows[i]);
+//             }
+//         }
+//         if (minimized.length > 0) {
+//             reportMinimizedWindows(minimized);
+//         }
+//     });
+// }, 5000);
